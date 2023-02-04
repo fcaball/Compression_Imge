485d046a4f246725d7c4f3fb9d5f930d38b5942e
fabienCaballero <fabest.09@orange.fr>
2023-02-04 16:27:25 +0100

    fin tp3

--- TP3/TP3.cpp
+++ TP3/TP3.cpp
@@ -3,10 +3,12 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char *argv[])
 {
   char cNomImgLue[250];
-  int nH, nW, nTaille;
+  int nH, nW, nTaillle;
 
   if (argc != 3)
   {
@@ -18,10 +20,10 @@ int main(int argc, char *argv[])
   OCTET *ImgIn, *ImgOut;
 
   lire_nb_lignes_colonnes_image_pgm(cNomImgLue, &nH, &nW);
-  nTaille = nH * nW;
+  nTaillle = nH * nW;
 
-  allocation_tableau(ImgIn, OCTET, nTaille);
-  allocation_tableau(ImgOut, OCTET, nTaille);
+  allocation_tableau(ImgIn, OCTET, nTaillle);
+  allocation_tableau(ImgOut, OCTET, nTaillle);
   lire_image_pgm(cNomImgLue, ImgIn, nH * nW);
 
   int *OCCGRIS;
@@ -37,7 +39,7 @@ int main(int argc, char *argv[])
   double p_i = 0.0;
   for (int i = 0; i < 256; i++)
   {
-    p_i = (double)OCCGRIS[i] / (double)nTaille;
+    p_i = (double)OCCGRIS[i] / (double)nTaillle;
     if (p_i != 0)
       entropie -= (p_i)*log2(p_i);
   }
@@ -55,22 +57,44 @@ int main(int argc, char *argv[])
   //       ImgOut[i * nW + j] = (ImgIn[i * nW + j - 1] - ImgIn[i * nW + j]) + 128;
   //   }
   // }
+  double E = 0;
 
-  for (size_t i = 0; i < nW; i++)
-  {
+  // for (size_t i = 0; i < nTaillle; i++)
+  // {
+ 
 
-    for (size_t j = 0; j < nH; j++)
+  //   // OCTET r = (((ImgIn[i * nW + j - 1] + ImgIn[i * nW + j - 1]) / 2) - ImgIn[i * nW + j]);
+  //   // ImgOut[i * nW + j] = r + 128;
+  // }
+
+
+  for (int i = 0; i < nH; i++)
     {
-      OCTET r = (((ImgIn[i * nW + j - 1] + ImgIn[i * nW + j - 1]) / 2) - ImgIn[i * nW + j]);
-      ImgOut[i * nW + j] = r + 128;
+        for (int j = 0; j < nW; j++)
+        {
+            int minvois = min((int)ImgIn[i * nW + j - 1], (int)ImgIn[i * nW + j - nW]);
+            int maxvois = max((int)ImgIn[i * nW + j - 1], (int)ImgIn[i * nW + j - nW]);
+
+            if ((int)ImgIn[i * nW + j - nW -1] >= maxvois)
+            {
+                ImgOut[i * nW + j] = minvois - ImgIn[i * nW + j] + 128;
+            }
+            else if ((int)ImgIn[i * nW + j - nW - 1] <= minvois)
+            {
+                ImgOut[i * nW + j] = maxvois - ImgIn[i * nW + j] + 128;
+            }
+            else
+            {
+                ImgOut[i * nW + j] = (ImgIn[i * nW + j - 1] + ImgIn[i * nW + j - nW] - ImgIn[i * nW + j - nW - 1]) - ImgIn[i * nW + j] + 128;
+            }
+        }
     }
-  }
 
   // Taux de compression : 1.79 pour l'image d'erreur de pred
 
-  histoGrey(ImgIn, nTaille);
+  histoGrey(ImgOut, nTaillle);
 
-  // ecrire_image_pgm((char *)argv[2], ImgOut, nW, nH);
+  ecrire_image_pgm((char *)argv[2], ImgOut, nW, nH);
 
   return 0;
 }
